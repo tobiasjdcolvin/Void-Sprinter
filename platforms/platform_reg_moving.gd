aeeffc2ffb9f087123f68723f44ee310e6f63283
Tobias Colvin <107777392+tobiasjdcolvin@users.noreply.github.com>
2024-05-14 16:50:29 -0700

    Player can now move with moving platforms

--- platforms/platform_reg_moving.gd
+++ platforms/platform_reg_moving.gd
@@ -1,28 +1,31 @@
 extends RigidBody3D
 
+var move_speed = 7
 @onready var to_rotate_towards
 @onready var move_direc = "right"
 @onready var falling = false
 @onready var direc_tween
 @onready var has_rotated = false
 
-
+func _ready():
+	linear_velocity.x = move_speed
+	move_direc = "left"
+	
 
 func _on_move_timer_timeout():
 	if falling == false:
-		direc_tween = get_tree().create_tween()
 		# move right, set left
 		if move_direc == "right":
-			direc_tween.tween_property(self, "position", Vector3(self.position.x + 10, self.position.y, self.position.z), 2)
+			linear_velocity.x = move_speed
 			move_direc = "left"
 		# move left, set right
 		else:
-			direc_tween.tween_property(self, "position", Vector3(self.position.x - 10, self.position.y, self.position.z), 2)
+			linear_velocity.x = -move_speed
 			move_direc = "right"
 
 func _physics_process(_delta):
 	if falling:
-		direc_tween.pause()
+		linear_velocity.x = 0
 
 func rotate_now():
 	var rotate_tween = get_tree().create_tween()
